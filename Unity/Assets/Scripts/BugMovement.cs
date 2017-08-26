d4900576a677f7b1c77742c824902b1ad4aa92d1
Alberto <albertoesbueno@hotmail.com>
2017-08-26 13:13:41 +0100

    GameControl y BugMovement

---
+++ Unity/Assets/Scripts/BugMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugMovement : MonoBehaviour {
+
+	public float velocity = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = GameControl.main().computerPosition();
+		transform.position -= Vector3.Normalize(transform.position - position) * velocity * Time.deltaTime;
+	}
+}
--- Unity/Assets/Scripts/GameControl.cs
+++ Unity/Assets/Scripts/GameControl.cs
@@ -7,11 +7,23 @@ public class GameControl : MonoBehaviour {
 	public GameObject computerPrefab;
 	public GameObject bugPrefab;
 
-	private int nummberOfBugs;
+	public int bugWaveAmount = 4;
+	public int numberOfComputers = 4;
+
+	private int numberOfBugs;
+	private int computersLeft;
+
+	public List<Transform> spawnPositions;
+
+	private static GameControl mainControl;
+	private GameObject computer;
 
 	// Use this for initialization
 	void Start () {
-		
+		numberOfBugs = bugWaveAmount;
+		computersLeft = numberOfComputers;
+		mainControl = this;
+		AddComputer();
 	}
 	
 	// Update is called once per frame
@@ -19,10 +31,36 @@ public class GameControl : MonoBehaviour {
 		
 	}
 
-	public void AddComputer() {
-
+	public static GameControl main() {
+		return mainControl;
 	}
 
-	
+	public void AddComputer() {
+		Transform computerTranform = spawnPositions[Random.Range(0, spawnPositions.Count)];
+		computer = Instantiate(computerPrefab, computerTranform.position, Quaternion.identity);
+		for(int i = 0; i < numberOfBugs; i++) {
+			Transform newTranform;
+			do{
+				newTranform = spawnPositions[Random.Range(0, spawnPositions.Count)];
+			}while(newTranform.position == computerTranform.position);
+			Instantiate(bugPrefab, newTranform.position, Quaternion.identity);
+		}
+	}
+
+	public void BugDie(){
+		numberOfBugs--;
+		if(numberOfBugs == 0) {
+			if(computersLeft == 0) {
+				//win
+			}else{
+				computersLeft--;
+				AddComputer();
+			}
+		}
+	}
+
+	public Vector3 computerPosition() {
+		return computer.transform.position;
+	}
 
 }
