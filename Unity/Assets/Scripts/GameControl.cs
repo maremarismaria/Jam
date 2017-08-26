6092ede3db9ce75257ce1a301260a9c37841af09
Alberto <albertoesbueno@hotmail.com>
2017-08-26 12:14:54 +0100

    Graphics

---
+++ Unity/Assets/Scripts/GameControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+	public GameObject computerPrefab;
+	public GameObject bugPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddComputer() {
+
+	}
+
+
+
+}
