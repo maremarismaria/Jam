45e61de234adf18652c6e9eca6d9029f62155b91
Alberto <albertoesbueno@hotmail.com>
2017-08-27 10:11:19 +0100

    Move around

---
+++ Unity/Assets/MoveAround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAround : MonoBehaviour {
+
+	public MoveAround otherside;
+	public bool isTheRightSide = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.CompareTag("Player") || other.CompareTag("Bug")) {
+			otherside.MoveHere(other.gameObject);
+		}
+	}
+
+	public void MoveHere(GameObject toMove){
+		float offset = toMove.GetComponent<Collider2D>().bounds.size.x;
+		Vector3 toMovePosition = toMove.transform.position;
+		float x = isTheRightSide? GetComponent<BoxCollider2D>().bounds.min.x - offset : GetComponent<BoxCollider2D>().bounds.max.x + offset;
+		toMove.transform.position = new Vector3(x, toMovePosition.y, toMovePosition.z);
+	}
+
+}
