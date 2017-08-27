81c7e4d72d71d6f5fb8093fa0680b2254aa88e32
Alberto <albertoesbueno@hotmail.com>
2017-08-27 13:08:54 +0100

    Start Anim

---
+++ Unity/Assets/ComputerAnim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerAnim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.CompareTag("Player") && !GameControl.Main().IsStarted()) {
+			GameControl.Main().GetPlayer().SetActive(false);
+			GetComponent<Animator>().SetBool(GlobalVariables.playerSelected? "GirlAnim" : "BoyAnim", true);
+			StartCoroutine(CodingAnimationCorutine());
+		}
+
+	}
+
+	IEnumerator CodingAnimationCorutine() {
+		yield return new WaitForSeconds(3);
+		GameControl.Main().GetPlayer().SetActive(true);
+		GetComponent<Animator>().SetBool(GlobalVariables.playerSelected? "GirlAnim" : "BoyAnim", false);
+		GameControl.Main().SpawnBugs();
+	}
+
+}
